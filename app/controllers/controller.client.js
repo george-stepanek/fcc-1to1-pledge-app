9781255d2e8a664b12893b0657ab47f68e3031cd
stepang <g.stepanek.nz@gmail.com>
2016-02-11 22:53:10 +0000

    Add basic very display of pledges to front end

--- app/controllers/controller.client.js
+++ app/controllers/controller.client.js
@@ -1,11 +1,26 @@
-$("document").ready($.get(window.location.origin + '/api/:id', function (user) {
-	var name = (user.displayName !== null ? user.displayName : user.username);
-	
-	ReactDOM.render(
-		<div>
-			<User name={name}/>
-			<a href="/logout">Logout</a>
-		</div>,
-		document.getElementById('content')
-	);
-}));
+$("document").ready(
+	$.get(window.location.origin + '/api/:id', function (user) {
+		var name = (user.displayName !== null ? user.displayName : user.username);
+		
+		$.get(window.location.origin + '/api/pledges/all', function (pledges) {
+			var pledgeNodes = pledges.map(function(pledge) {
+				return (
+					<p key={pledge._id}>
+						<img src={pledge.smallImageUrl}/>
+						<br/>
+						{pledge.title}
+					</p>
+				);
+			});
+			
+			ReactDOM.render(
+				<div>
+					<User name={name}/>
+					{pledgeNodes}
+					<a href="/logout">Logout</a>
+				</div>,
+				document.getElementById('content')
+			);
+		});
+	})
+);
