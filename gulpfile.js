c24f9c452766df8cd0fbd9819fbc0e70b7dc12e7
stepang <g.stepanek.nz@gmail.com>
2016-02-16 02:45:27 +0000

    Implement gulp seed task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var react = require('gulp-react');
+var mongodbData = require('gulp-mongodb-data');
+require('dotenv').load();
 
 var path = {
   JS: ['app/components/*.js*'],
   CONCAT_FILE: 'build.js',
   DEST: 'public/scripts'
 };
+
+gulp.task('seed', function() {
+  gulp.src('app/seed/pledges.json')
+    .pipe(mongodbData({
+      mongoUrl: process.env.MONGO_URI,
+      collectionName: 'pledges',
+      dropCollection: true
+    }));
+});
 
 gulp.task('transform', function(){
   gulp.src(path.JS)
