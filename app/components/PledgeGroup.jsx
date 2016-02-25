c68f8654cd0f73aa113a14c3ac67491585c25e39
stepang <g.stepanek.nz@gmail.com>
2016-02-25 00:44:40 +0000

    Tidy

--- app/components/PledgeGroup.jsx
+++ app/components/PledgeGroup.jsx
@@ -1,8 +1,15 @@
 var PledgeGroup = React.createClass({
     render: function() {
-		var pledgeNodes = this.props.pledges.map(function(pledge) {
-			return ( <PledgeThumbnail pledge={pledge} key={pledge._id} /> );
+		var pledges = this.props.pledges.map(function(pledge) {
+			return (
+				<div className="pledgeLink" key={pledge._id}>
+					<a href={"/pledge?id=" + pledge._id}>
+						<img src={pledge.thumbnailUrl}/>
+						<h4 className="pledgeTitle"><span>{pledge.title.toUpperCase()}</span></h4>
+					</a>
+				</div>
+			);
 		});
-        return ( <div>{pledgeNodes}</div> );
+        return ( <div>{pledges}</div> );
 	}
 });
