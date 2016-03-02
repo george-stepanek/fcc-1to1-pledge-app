3e162466a23779c0f60a479b374131b10d4acfe7
stepang <g.stepanek.nz@gmail.com>
2016-03-02 21:09:34 +0000

    Add (greeked) intro text to main page

--- app/components/MainPage.jsx
+++ app/components/MainPage.jsx
@@ -24,7 +24,7 @@ var MainPage = React.createClass({
     render: function() {
 		var categories = this.state.categories.map(function(category) {
 			return (
-				<div className="pledge-link col-lg-3 col-md-4 col-sm-6" key={category.title}>
+				<div className="pledge-link col-lg-2 col-md-4 col-sm-6" key={category.title}>
 					<a href={"/category/" + category.title}>
 						<img src={category.imageUrl}/>
 						<h4 className="pledge-thumb-title">
@@ -37,7 +37,14 @@ var MainPage = React.createClass({
 		return (
             <div>
                 <Header />
-        		<div>
+				<div className="none-found">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+					Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+					Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+					Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</div>
+        		<div className="row">
+        			<div className="col-lg-1"/>
                     {categories}
         		</div>
     		</div>
