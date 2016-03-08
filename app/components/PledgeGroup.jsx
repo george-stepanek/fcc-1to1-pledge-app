6d11bc815e4adbc3dcad45ab964ac79eba89ffa9
stepang <g.stepanek.nz@gmail.com>
2016-03-08 21:06:08 +0000

    Improve truncate pledge explanation

--- app/components/PledgeGroup.jsx
+++ app/components/PledgeGroup.jsx
@@ -8,12 +8,17 @@ var PledgeGroup = React.createClass({
     },
     render: function() {
 		var pledges = this.props.pledges.map(function(pledge) {
+			var explanation = pledge.explanation.slice(0, 80);
+			if(explanation.length == 80) {
+				explanation = explanation.slice(0, explanation.lastIndexOf(' ')) + "…";
+			}
+			
 			return (
 				<div className="pledge-link col-lg-3 col-md-4 col-sm-6" key={pledge._id}>
 					<a href={"/pledge/" + pledge.title.toLowerCase().replace(/\s/g, "-")}>
 						<img src={pledge.thumbnailUrl}/>
 						<h4 className="pledge-thumb-title"><span>{pledge.title}</span></h4>
-						<h4 className="pledge-info">{pledge.explanation.slice(0, 100)}&hellip;</h4>
+						<h4 className="pledge-info">{explanation}</h4>
 					</a>
 				</div>
 			);
