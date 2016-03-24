cc41954bf7d3c2ad0213cf64180eda20e302eb4b
stepang <g.stepanek.nz@gmail.com>
2016-03-24 01:39:32 +0000

    Improve gulp seed to update database not just delete/reload

--- gulpfile.js
+++ gulpfile.js
@@ -9,15 +9,7 @@ var path = {
   CONCAT_FILE: 'build.js',
   DEST: 'public/scripts'
 };
-
-gulp.task('seed', function() {
-  gulp.src('app/seed/pledges.json')
-    .pipe(mongodbData({
-      mongoUrl: process.env.MONGO_URI,
-      collectionName: 'pledges',
-      dropCollection: true
-    }));
-});
+var seed = './app/seed/pledges.json';
 
 gulp.task('transform', function(){
   gulp.src(path.JS)
@@ -31,3 +23,36 @@ gulp.task('watch', function(){
 });
 
 gulp.task('default', ['watch']);
+
+gulp.task('seed', function() {
+  var mongoose = require('mongoose');
+  mongoose.connect(process.env.MONGO_URI);
+  var Pledge = require('./app/models/pledges.js');
+  
+  var counter = 0, pledgesData = require(seed);
+  pledgesData.forEach(function(pledgeData) {
+		Pledge.findOne({ 'title': pledgeData.title }, function (err, pledge) {
+	    if (err) { throw err; }
+			if (!pledge) {
+			  pledge = new Pledge();
+			  pledge.title = pledgeData.title;
+			}
+
+      pledge.category = pledgeData.category;
+      pledge.explanation = pledgeData.explanation;
+      pledge.impactPerWeek = pledgeData.impactPerWeek;
+      pledge.impactUnits = pledgeData.impactUnits;
+      pledge.source = pledgeData.source;
+      pledge.citation = pledgeData.citation;
+      pledge.imageUrl = pledgeData.imageUrl;
+      pledge.thumbnailUrl = pledgeData.thumbnailUrl;
+
+			pledge.save(function (err) {
+			  if(++counter == pledgesData.length) {
+			    mongoose.connection.close();			    
+			  }
+				if (err) { throw err; }
+			});
+		});
+  });
+});
